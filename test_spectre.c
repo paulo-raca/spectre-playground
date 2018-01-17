23f7c04190a0fab4bba774e7bcf331379b64c0fe
Paulo Costa <me@paulo.costa.nom.br>
2018-01-17 15:48:35 -0200

    Minor optimizations and cleanup

--- test_spectre.c
+++ test_spectre.c
@@ -10,45 +10,26 @@
 #include <x86intrin.h> /* for rdtscp and clflush */
 #endif
 
+#define ALIGN_CACHE __attribute__ ((aligned (512)))
+
 /********************************************************************
 Victim code.
 ********************************************************************/
-unsigned int array1_size = 16;
-uint8_t unused1[64];
-uint8_t array1[160] = {
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16
-};
-uint8_t unused2[64];
-uint8_t array2[256 * 512];
-
-char * secret = "The Magic Words are Squeamish Ossifrage.";
-
-uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
+uint8_t    ALIGN_CACHE array1[512];
+uint32_t   ALIGN_CACHE array1_size = sizeof(array1);
+uint8_t    ALIGN_CACHE array2[256 * 512];
 
 void victim_function(size_t x) {
   if (x < array1_size) {
-    temp &= array2[array1[x] * 512];
+    array2[array1[x] * 512] = 0;
   }
 }
 
 /********************************************************************
 Analysis code
 ********************************************************************/
+
+/* TODO: How is it affected by frequency scaling? */
 #define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
